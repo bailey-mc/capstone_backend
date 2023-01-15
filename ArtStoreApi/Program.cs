eb6db02f3866cdce3e86a73ed00833b223b98cba
Bailey McDonald <mcdonald.bailey@gmail.com>
2023-01-15 14:52:06 -0600

    configured cors in program.cs so frontend can access api

--- ArtStoreApi/Program.cs
+++ ArtStoreApi/Program.cs
@@ -1,8 +1,21 @@
 using ArtStoreApi.Models;
 using ArtStoreApi.Services;
+//enable cors so frontend app can pull
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//builder for CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:3000");
+                      });
+});
 
 //tells artstoredatabasesettings totake porperties from artstoredatabase properties in appsettings.json
 builder.Services.Configure<ArtStoreDatabaseSettings>(
@@ -25,6 +38,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//for cors requests
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
--- README.md
+++ README.md
@@ -10,3 +10,10 @@ https://code.visualstudio.com/docs/containers/quickstart-aspnet-core
 
 Docker to Heroku Tutorial
 https://medium.com/bina-nusantara-it-division/how-to-deploy-your-asp-net-core-web-api-on-heroku-using-docker-a21409881d76#:~:text=Currently%2C%20Heroku%20supports%20languages%20Ruby,in%20order%20to%20deploy%20our%20.
+made precite bookstore api w/ docker thru vscode docker tutorial... but crashed on heroku
+failed ... something missing in dockerfile
+
+tried this buildpack
+https://github.com/jincod/dotnetcore-buildpack
+doesn't work w/ mongo
+giving up on deploying to heroku for the moment... may try deploying to azure later on
